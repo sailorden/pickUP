b249bc653e403749dbc28db33da3cc1b13745ff7
Todd Kronenberg <toddkronenberg@gmail.com>
2015-04-27 23:25:00 -0700

    Get rudimentary accordian list working in dash

--- www/views/dashboard/dashboardController.js
+++ www/views/dashboard/dashboardController.js
@@ -4,15 +4,41 @@ angular.module('pickUp')
 
 .controller('DashboardCtrl', ['$scope', function($scope) {
 
+  $scope.pickUpShown = null;
   $scope.pickUpLines = [
     {
+      _id: '001',
       line: "Let's act like a calendar and date!",
       date: '04/27 7:42pm',
       numberOfResponses: 8,
       lat: '37',
-      long: '-122'
+      long: '-122',
+      responses: [
+        {
+          name: 'Lisa',
+          image: 'http://freedwallpaper.com/wp-content/uploads/2014/12/Best-Cute-Girls-Wallpapers-HD-Desktop.jpg',
+          age: '29',
+          location: 'Palo Alto, CA',
+          response: 'I like that.'
+        },
+        {
+          name: 'Joanie',
+          image: 'http://freedwallpaper.com/wp-content/uploads/2014/04/8589130465188-beautiful-faces-cute-girls-wallpaper-hd.jpg',
+          age: '31',
+          location: 'Mountain View, CA',
+          response: 'Yeah man'
+        },
+        {
+          name: 'Sara',
+          image: 'http://media.doisongphapluat.com/247/2015/1/16/gai-xinh.jpg',
+          age: '24',
+          location: 'San Francisco, CA',
+          response: "Let's do it!"
+        }
+      ]
     },
     {
+      _id: '002',
       line: "You must be from Tennessee, cuz you're the only 10 I see!",
       date: '04/25 9:20am',
       numberOfResponses: 19,
@@ -20,6 +46,7 @@ angular.module('pickUp')
       long: '-77.3464'
     },
     {
+      _id: '003',
       line: "Once upon a time I went to market and on the way home I met a man who said he was a genie and I took him at his word.",
       date: '04/24 11:30am',
       numberOfResponses: 173,
@@ -28,28 +55,18 @@ angular.module('pickUp')
     },
   ];
 
-  $scope.views = [
-    {
-      name: 'Lisa',
-      image: 'http://freedwallpaper.com/wp-content/uploads/2014/12/Best-Cute-Girls-Wallpapers-HD-Desktop.jpg',
-      age: '29',
-      location: 'Palo Alto, CA',
-      response: 'I like that.'
-    },
-    {
-      name: 'Joanie',
-      image: 'http://freedwallpaper.com/wp-content/uploads/2014/04/8589130465188-beautiful-faces-cute-girls-wallpaper-hd.jpg',
-      age: '31',
-      location: 'Mountain View, CA',
-      response: 'Yeah man'
-    },
-    {
-      name: 'Sara',
-      image: 'http://media.doisongphapluat.com/247/2015/1/16/gai-xinh.jpg',
-      age: '24',
-      location: 'San Francisco, CA',
-      response: "Let's do it!"
+  $scope.togglePickUp = function(pickUpID) {
+    if ($scope.pickUpShown === pickUpID) {
+      $scope.pickUpShown = null;
+    } else {
+      $scope.pickUpShown = pickUpID;
     }
-  ];
+    console.log('pickupShown:', $scope.pickUpShown);
+    console.log('pickupID:', pickUpID)
+  };
+
+  $scope.isPickUpShown = function(pickUpID) {
+    return $scope.pickUpShown === pickUpID;
+  };
 
 }]);
