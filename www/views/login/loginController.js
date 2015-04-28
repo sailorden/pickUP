b7ec5b77d8221a08c5d243795f9aa75277f4840a
Todd Kronenberg <toddkronenberg@gmail.com>
2015-04-27 17:10:21 -0700

    Add fb login, but not working, so comment out code Left facebook login button but it just routes to the dashboard instead of logging in to facebook

--- www/views/login/loginController.js
+++ www/views/login/loginController.js
@@ -4,8 +4,21 @@ angular.module('pickUp')
 
 .controller('LoginCtrl', ['$scope', '$state', function($scope, $state) {
 
-  setTimeout(function() {
-    $state.go('app.dashboard')
-  }, 2000);
+  var ref = new Firebase("https://pickupapp.firebaseio.com");
+
+  // setTimeout(function() {
+  //   $state.go('app.dashboard')
+  // }, 2000);
+
+  $scope.login = function() {
+    // ref.authWithOAuthPopup("facebook", function(error, authData) {
+    //   if (error) {
+    //     console.log("Login Failed!", error);
+    //   } else {
+    //     console.log("Authenticated successfully with payload:", authData);
+    //   }
+    // });
+    $state.go('app.dashboard');
+  };
 
 }]);
